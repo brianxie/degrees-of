574c2ab683acb98cf30a8ddf8903a93cff090ed3
Brian Xie <briancxie@gmail.com>
2016-07-23 19:55:25 -0700

    more empty structure

--- backend/flask/flask-server.py
+++ backend/flask/flask-server.py
@@ -1,2 +1,11 @@
 from flask import Flask
 from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "brian\nkevin\nrahul"
+
+if __name__ == "__main__":
+    app.run()
