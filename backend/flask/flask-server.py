f0391b45b812c60c42f5cb47848e81dd405273d8
Brian Xie <briancxie@gmail.com>
2016-07-24 06:48:30 -0700

    flask uris working, but make_connection seems broken

--- backend/flask/flask-server.py
+++ backend/flask/flask-server.py
@@ -10,15 +10,33 @@ app = Flask(__name__)
 def root():
     return "brian\nkevin\nrahul"
 
+
+@app.route("/getname/<uuid>")
+def getName(uuid):
+    return json.dumps(get_name(uuid))
+
+@app.route("/createuserdata/<name>/<uuid>/<isartist>")
+def createUserData(name, uuid, isartist):
+    return json.dumps(create_user_data(name, uuid, isartist))
+
+@app.route("/getuserentry/<uuid>")
+def getUserEntry(uuid):
+    return json.dumps(get_user_entry(uuid))
+
+@app.route("/getartistscores/<uuid>")
+def getArtistScores(uuid):
+    user = get_user_entry(uuid)
+    return json.dumps(get_artist_scores(user))
+
+@app.route("/makeconnection/<uuid1>/<uuid2>")
+def makeConnection(uuid1, uuid2):
+    return make_connection(uuid1, uuid2)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 
 
-#getname
-#createuserdata
-#getuserentry
-#getartistscores
-#makeconnection
 
 # dead
 # def recompute_graph(): # not required if we graph is built incrementally (?)
