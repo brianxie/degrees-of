81803af12a9633e8db2a4c665a4a66dc9ee2fadf
Brian Xie <briancxie@gmail.com>
2016-07-24 07:09:17 -0700

    something broken...pushing to avoid merge conflict for testing

--- backend/backend-tests.py
+++ backend/backend-tests.py
@@ -5,38 +5,51 @@ sys.path.append("./flask/")
 from flasklib import *
 
 # constants
-UUID_1 = "0000000000"
-UUID_2 = "9999999999"
+UUID_1 = 1
+UUID_2 = 2
+UUID_3 = 3
 
 client = MongoClient()
 users = client.users # database users
 names = client.names # database names
 
-USER_1 = create_user_data("brian", UUID_1, False)
-print(USER_1)
-USER_2 = create_user_data("kanye", UUID_2, True)
-print(USER_2)
-result = get_user_entry(UUID_1)
-print(result)
-result = get_user_entry(UUID_2)
-print(result)
+USER_1 = create_user_data("kevin", UUID_1, True)
+USER_2 = create_user_data("rahul", UUID_2, True)
+USER_3 = create_user_data("brian", UUID_3, False)
 
-result = get_artist_scores(USER_1)
-print(result)
-result = get_artist_scores(USER_2)
-print(result)
+make_connection(USER_1, USER_3)
+make_connection(USER_2, USER_3)
 
-result = make_connection(UUID_1, UUID_2)
-
-USER_1 = get_user_entry(UUID_1)
-print(get_artist_scores(USER_1))
-print(get_artist_scores(USER_2))
-
-print(get_all_users())
+for thing in get_all_users():
+    print(thing)
+    print("================")
 
 
-print(get_name(UUID_1))
-print(get_name(UUID_2))
+# USER_1 = create_user_data("brian", UUID_1, False)
+# print(USER_1)
+# USER_2 = create_user_data("kanye", UUID_2, True)
+# print(USER_2)
+# result = get_user_entry(UUID_1)
+# print(result)
+# result = get_user_entry(UUID_2)
+# print(result)
+
+# result = get_artist_scores(USER_1)
+# print(result)
+# result = get_artist_scores(USER_2)
+# print(result)
+
+# result = make_connection(UUID_1, UUID_2)
+
+# USER_1 = get_user_entry(UUID_1)
+# print(get_artist_scores(USER_1))
+# print(get_artist_scores(USER_2))
+
+# print(get_all_users())
+
+
+# print(get_name(UUID_1))
+# print(get_name(UUID_2))
 
 
 # result = get_user_entry(UUID_1)
