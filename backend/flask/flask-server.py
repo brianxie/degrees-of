fc57fb8a5040c8e8d1f467abf8661792c6a54be0
Brian Xie <briancxie@gmail.com>
2016-07-24 10:25:46 -0700

    testing for flask requests; everything seems to work, except for makeConnection()

--- backend/flask/flask-server.py
+++ backend/flask/flask-server.py
@@ -8,49 +8,58 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def root():
-    if request.method == "GET":
-        print("get")
-    elif request.method == "POST":
-        print("post")
-    else:
-        print("no")
     # print(json.loads(request.data))
-    print(request.data)
-
+    # print(request.data)
     return "brian\nkevin\nrahul"
 
-# TODO http requests
-
 # @app.route("/getname/<uuid>")
+# def getName(uuid):
 @app.route("/getname/", methods=["GET"])
-def getName(uuid):
+def getName():
+    query = request.args
+    uuid = query["uuid"]
     return json.dumps(get_name(uuid))
 
 # @app.route("/getuserentry/<uuid>")
+# def getUserEntry(uuid):
 @app.route("/getuserentry/", methods=["GET"])
-def getUserEntry(uuid):
+def getUserEntry():
+    query = request.args
+    uuid = query["uuid"]
     return json.dumps(get_user_entry(uuid))
 
 # @app.route("/getartistscores/<uuid>")
+# def getArtistScores(uuid):
 @app.route("/getartistscores/", methods=["GET"])
-def getArtistScores(uuid):
+def getArtistScores():
+    query = request.args
+    uuid = query["uuid"]
     user = get_user_entry(uuid)
     return json.dumps(get_artist_scores(user))
 
 # @app.route("/createuserdata/<name>/<uuid>/<isartist>")
+# def createUserData(name, uuid, isartist):
 @app.route("/createuserdata/", methods=["POST"])
-def createUserData(name, uuid, isartist):
-    return json.dumps(create_user_data(name, uuid, isartist))
+def createUserData():
+    query = request.form.to_dict()
+    name = query["name"]
+    uuid = query["uuid"]
+    is_artist = query["is_artist"]
+    return json.dumps(create_user_data(name, uuid, is_artist))
 
 # @app.route("/makeconnection/<uuid1>/<uuid2>")
-@app.route("/makeconnection/", methods=["POST"])
-def makeConnection(uuid1, uuid2):
-    return make_connection(uuid1, uuid2)
+# def makeConnection(uuid1, uuid2):
+@app.route("/makeconnection/", methods=["GET"])
+def makeConnection():
+    query = request.form.to_dict()
+    print(query)
+    uuid_1 = query["uuid_1"]
+    uuid_2 = query["uuid_2"]
+    return json.dumps(make_connection(uuid_1, uuid_2))
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
 
 
 # dead
---
+++ backend/http-tests.py
@@ -0,0 +1,65 @@
+import requests
+
+root_address = "http://10.123.147.139:5000/"
+
+create_user_data_address = root_address + "createuserdata/"
+create_user_data_payload_1 = {"name": "lana", "uuid": 1, "is_artist": True}
+create_user_data_payload_2 = {"name": "kanye", "uuid": 2, "is_artist": True}
+create_user_data_payload_3 = {"name": "rahul", "uuid": 3, "is_artist": False}
+
+r1 = requests.post(create_user_data_address, create_user_data_payload_1)
+print(r1.text)
+r2 = requests.post(create_user_data_address, create_user_data_payload_2)
+print(r2.text)
+r3 = requests.post(create_user_data_address, create_user_data_payload_3)
+print(r3.text)
+
+print()
+
+get_user_entry_address = root_address + "getuserentry"
+get_user_entry_payload_1 = {"uuid": 1}
+get_user_entry_payload_2 = {"uuid": 2}
+get_user_entry_payload_3 = {"uuid": 3}
+
+r1 = requests.get(get_user_entry_address, get_user_entry_payload_1)
+print(r1.text)
+r2 = requests.get(get_user_entry_address, get_user_entry_payload_2)
+print(r2.text)
+r3 = requests.get(get_user_entry_address, get_user_entry_payload_3)
+print(r3.text)
+
+print()
+
+get_artist_scores_address = root_address + "getartistscores"
+get_artist_scores_payload_1 = {"uuid": 1}
+get_artist_scores_payload_2 = {"uuid": 2}
+get_artist_scores_payload_3 = {"uuid": 3}
+
+r1 = requests.get(get_artist_scores_address, get_artist_scores_payload_1)
+print(r1.text)
+r2 = requests.get(get_artist_scores_address, get_artist_scores_payload_2)
+print(r2.text)
+r3 = requests.get(get_artist_scores_address, get_artist_scores_payload_3)
+print(r3.text)
+
+print()
+
+get_name_address = root_address + "getname"
+get_name_payload_1 = {"uuid": 1}
+get_name_payload_2 = {"uuid": 2}
+get_name_payload_3 = {"uuid": 3}
+
+r1 = requests.get(get_name_address, get_name_payload_1)
+print(r1.text)
+r2 = requests.get(get_name_address, get_name_payload_2)
+print(r2.text)
+r3 = requests.get(get_name_address, get_name_payload_3)
+print(r3.text)
+
+print()
+
+make_connection_address = root_address + "makeconnection"
+make_connection_payload_1 = {"uuid_1": 1, "uuid_2": 2}
+
+r1 = requests.post(make_connection_address, make_connection_payload_1)
+print(r1.text)
