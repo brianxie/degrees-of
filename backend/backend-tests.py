fec48ec1a49a6813a963082ec2387cec49cfeda4
Brian Xie <briancxie@gmail.com>
2016-07-24 00:05:58 -0700

    some flasklib updates, user-data-schema update (forgot to include distance lol)

--- backend/backend-tests.py
+++ backend/backend-tests.py
@@ -4,27 +4,38 @@ import sys
 sys.path.append("./flask/")
 from flasklib import *
 
+# constants
 UUID_1 = "0000000000"
 UUID_2 = "9999999999"
 
 client = MongoClient()
-
 users = client.users # database users
 # userinfo = client.userinfo # database userinfo
 
-
-# insert the data
-result = add_user(UUID_1, False)
+result = create_user_data(UUID_1, False)
 # print(result)
 
+result = add_user(UUID_1, False)
+# print(result)
 result = add_user(UUID_2, False)
 # print(result)
 
+
 result = get_user(UUID_1)
 # print(result)
-
 result = get_all_users()
 # print(result)
 
-result = create_user_data(UUID_1, False)
-# print(result)
+USER_1 = get_user(UUID_1)
+result = get_neighbors(USER_1)
+print(result)
+
+
+
+
+
+
+# get_distances
+# add_neighbor
+# update_node
+# make_connection
--- backend/flask/flasklib.py
+++ backend/flask/flasklib.py
@@ -1,6 +1,30 @@
 from pymongo import MongoClient
 import json
 
+def get_user(uuid): # queries users db
+    client = MongoClient()
+    users = client.users # database users
+    result = users.all.find_one({"_id": uuid}) # searches just by uuid; at most one match
+    return result
+
+def get_all_users(): # queries users
+    client = MongoClient()
+    users = client.users # database users
+    cursor = users.all.find()
+    documents = []
+    for document in cursor:
+        documents.append(document)
+    return documents
+
+def get_neighbors(user):
+    # retrieve list of neighbors (1-neighbors, a la adjacency list)
+    neighbors = user["neighbors"]
+    return neighbors
+
+def get_distances(user):
+    # get distances to all artists for given uuid
+    return None
+
 def create_user_data(uuid, is_artist):
     # initializes the (json? bson? python thing?) that characterizes a user
     # see user-data-schema
@@ -22,29 +46,6 @@ def add_user(uuid, is_artist): # string, bool; queries users
     result = users.all.update_one(user_data, {"$setOnInsert": user_data}, upsert=True)
     return result
 
-def get_user(uuid): # queries users
-    client = MongoClient()
-    users = client.users # database users
-    result = users.all.find_one({"_id": uuid}) # searches just by uuid; at most one match
-    return result
-
-def get_all_users(): # queries users
-    client = MongoClient()
-    users = client.users # database users
-    cursor = users.all.find()
-    documents = []
-    for document in cursor:
-        documents.append(document)
-    return documents
-
-def get_neighbors(user):
-    # retrieve list of neighbors (1-neighbors, a la adjacency list)
-    client = MongoClient()
-    return None
-
-def get_distances(user):
-    # get distances to all artists for given uuid
-    return None
 
 def add_neighbor(src, dst):
     # adds dst as a neighbor of src
